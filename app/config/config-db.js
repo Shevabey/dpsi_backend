1c8dc9c1ff78178c286672baf3b969aab5d626a3
Shevabey <shevabey100@gmail.com>
2024-07-19 14:49:23 +0700

    first commit

---
+++ app/config/config-db.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "bthnh66fotagpl9hales-mysql.services.clever-cloud.com",
+  USER: "u5wlp1f6zl8uynb4",
+  PASSWORD: "702CTTnSdMoftoqsFrKy",
+  DB: "bthnh66fotagpl9hales",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
