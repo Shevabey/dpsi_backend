c03e0619cc98e3c67fbeeec8d9f8f07f53f1a496
Shevabey <Shevabey100@gmail.com>
2024-07-22 18:07:25 +0700

    deploying

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ require("./app/routes/user-routes")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log("Running on port ", PORT);
+  console.log(`Running on port ${PORT}...`);
 });
